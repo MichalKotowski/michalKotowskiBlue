b69972c0d52f62f357d4fe9bf87db986517018a2
MichalKotowski <mzkotowski@gmail.com>
2023-06-18 20:49:15 +0200

    there's still so much work in front of me qq

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,9 +1,12 @@
 import Watch from '@components/Navigation/Watch'
 import NavigationItem from '@components/Navigation/NavigationItem'
 import SocialButton from './SocialIcon/SocialButton'
+import { useState } from 'react'
 import styles from './Navigation.module.scss'
 
 const Navigation = () => {
+	const [isToggled, setIsToggled] = useState(false)
+
 	return (
 		<nav className={styles.navigationWrapper}>
 			<Watch />
@@ -13,10 +16,13 @@ const Navigation = () => {
 				<NavigationItem path="/about" icon="about" label="About" />
 			</div>
 			<div className={styles.social}>
-				<SocialButton href="https://test.com" icon="github" />
-				<SocialButton href="https://test.com" icon="goodreads" />
-				<SocialButton href="https://test.com" icon="chess" />
-				<SocialButton href="https://test.com" icon="email" />
+				<SocialButton href="https://github.com/MichalKotowski" icon="github" />
+				<SocialButton
+					href="https://www.goodreads.com/review/list/70574245-micha-kotowski?shelf=read"
+					icon="goodreads"
+				/>
+				<SocialButton href="https://www.chess.com/member/ipisay" icon="chess" />
+				<SocialButton href="mailto:hello@michalkotowski.pl" icon="email" />
 			</div>
 		</nav>
 	)
