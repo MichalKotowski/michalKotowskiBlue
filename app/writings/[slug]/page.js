5f40550f2f4d2bc7aeab70146da5c1667245f326
Michal Kotowski <mzkotowski@gmail.com>
2023-07-04 20:23:25 +0200

    I'm so tired, but the project is going well

--- app/writings/[slug]/page.js
+++ app/writings/[slug]/page.js
@@ -1,18 +1,19 @@
-import { getWritingAndMoreWritings } from '../../../lib/api'
+import { getWriting } from '../../../lib/api'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import hljs from 'highlight.js/lib/core'
 import javascript from 'highlight.js/lib/languages/javascript'
-import { MARKS } from '@contentful/rich-text-types'
+import { MARKS, BLOCKS } from '@contentful/rich-text-types'
 import 'highlight.js/styles/tokyo-night-dark.css'
 import Tags from '@components/Tags'
 import Spacer from '@components/Spacer'
 import Flag from '@components/Flag'
 import Heading from '@components/Heading'
+import Image from 'next/image'
 
 hljs.registerLanguage('javascript', javascript)
 
 export async function generateMetadata({ params }) {
-	const { writing } = await getWritingAndMoreWritings(params.slug)
+	const { writing } = await getWriting(params.slug)
 
 	return {
 		title: `${writing.title} | Michał Kotowski`,
@@ -20,7 +21,7 @@ export async function generateMetadata({ params }) {
 }
 
 const Page = async ({ params }) => {
-	const { writing } = await getWritingAndMoreWritings(params.slug)
+	const { writing } = await getWriting(params.slug)
 
 	const options = {
 		renderMark: {
@@ -31,13 +32,16 @@ const Page = async ({ params }) => {
 
 				return (
 					<pre>
-						<code
-							dangerouslySetInnerHTML={markup}
-							class="hljs language-javascript"
-						></code>
+						<code className="hljs language-javascript">
+							<div dangerouslySetInnerHTML={markup}></div>
+						</code>
 					</pre>
 				)
 			},
+		},
+		renderNode: {
+			[BLOCKS.PARAGRAPH]: (node, children) => <div>{children}</div>,
+			// [BLOCKS.EMBEDDED_ASSET]: (node, children) => console.log('yuh')
 		},
 	}
 
