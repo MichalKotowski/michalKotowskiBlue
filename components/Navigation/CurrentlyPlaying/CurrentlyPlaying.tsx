b3f1cb8edd298f6430ffd5187bd3a4241b604a40
Michal Kotowski <mzkotowski@gmail.com>
2025-10-13 16:25:57 +0200

    Spotify widget improvements

--- components/Navigation/CurrentlyPlaying/CurrentlyPlaying.tsx
+++ components/Navigation/CurrentlyPlaying/CurrentlyPlaying.tsx
@@ -2,17 +2,32 @@ import styles from './CurrentlyPlaying.module.scss'
 import useSWR from 'swr'
 import Marquee from 'react-fast-marquee'
 import { useRef, useState, useEffect } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
 async function fetcher(url: RequestInfo | URL) {
 	return fetch(url).then((r) => r.json())
 }
 
 const CurrentlyPlaying = () => {
+	const isFirstRenderRef = useRef<boolean>(true)
 	const [isOverflow, setIsOverflow] = useState<boolean | undefined>(undefined)
 	const [windowWidth, setWindowWidth] = useState<number | undefined>(undefined)
+	const [isPlaying, setIsPlaying] = useState<boolean>(false)
 
 	const { data } = useSWR('/api/spotify/current', fetcher)
 	const ref = useRef<any>(null)
+
+	useEffect(() => {
+		isFirstRenderRef.current = false
+	}, [])
+
+	useEffect(() => {
+		if (data && data.isPlaying) {
+			setIsPlaying(true)
+		} else {
+			setIsPlaying(false)
+		}
+	}, [data])
 
 	useEffect(() => {
 		const handleWindowResize = () => {
@@ -40,53 +55,73 @@ const CurrentlyPlaying = () => {
 		}
 	}, [ref, data, windowWidth])
 
-	if (!data || !data.isPlaying) {
-		return (
-			<div className={`${styles.currentlyPlaying} ${styles.align}`}>
-				<div className={styles.spotify}></div>
-				<div className={styles.text}>
-					<p>Spotify playback is idle</p>
-				</div>
-			</div>
-		)
-	}
-
 	return (
-		<>
-			<p className={styles.hiddenText} ref={ref}>
-				<strong>{data.title}</strong> by <strong>{data.artist}</strong>
-			</p>
-			<div className={styles.currentlyPlaying}>
-				<div className={styles.icon}>
-					<span />
-					<span />
-					<span />
-				</div>
-				<div className={styles.text}>
-					{isOverflow ? (
-						<Marquee
-							pauseOnHover={true}
-							gradient={windowWidth && windowWidth < 981 ? false : true}
-							gradientColor={'rgb(5, 5, 7)'}
-							gradientWidth={20}
-							speed={30}
-						>
-							<a href={data.songUrl} target="_blank">
-								<strong>{data.title}</strong> by{' '}
-								<strong>
-									{data.artist}
-									<span>&nbsp;|&nbsp;</span>
-								</strong>
-							</a>
-						</Marquee>
-					) : (
-						<a href={data.songUrl} target="_blank">
-							<strong>{data.title}</strong> by <strong>{data.artist}</strong>
-						</a>
-					)}
-				</div>
-			</div>
-		</>
+		<AnimatePresence mode="wait">
+			{!isPlaying ? (
+				<motion.div
+					className={`${styles.currentlyPlaying} ${styles.align}`}
+					initial={
+						isFirstRenderRef.current
+							? { x: 0, opacity: 1 }
+							: { x: -15, opacity: 0 }
+					}
+					animate={{ x: 0, opacity: 1 }}
+					exit={{ x: 15, opacity: 0 }}
+					transition={{ duration: 0.3, ease: 'easeInOut' }}
+					key="not-playing"
+				>
+					<div className={styles.spotify}></div>
+					<div className={styles.text}>
+						<p>Spotify playback is idle</p>
+					</div>
+				</motion.div>
+			) : (
+				<motion.div
+					initial={{ x: -15, opacity: 0 }}
+					animate={{ x: 0, opacity: 1 }}
+					exit={{ x: 15, opacity: 0 }}
+					transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+					key="playing"
+				>
+					<p className={styles.hiddenText} ref={ref}>
+						<strong>{data.title}</strong> by <strong>{data.artist}</strong>
+					</p>
+					<div className={styles.currentlyPlaying}>
+						<div className={styles.iconWrapper}>
+							<div className={styles.icon}>
+								<span />
+								<span />
+								<span />
+							</div>
+						</div>
+						<div className={styles.text}>
+							{isOverflow ? (
+								<Marquee
+									pauseOnHover={true}
+									gradient={windowWidth && windowWidth < 981 ? false : true}
+									gradientColor={'rgb(5, 5, 7)'}
+									gradientWidth={20}
+									speed={30}
+								>
+									<a href={data.songUrl} target="_blank">
+										<strong>{data.title}</strong> by{' '}
+										<strong>
+											{data.artist}
+											<span>&nbsp;|&nbsp;</span>
+										</strong>
+									</a>
+								</Marquee>
+							) : (
+								<a href={data.songUrl} target="_blank">
+									<strong>{data.title}</strong> by{' '}
+									<strong>{data.artist}</strong>
+								</a>
+							)}
+						</div>
+					</div>
+				</motion.div>
+			)}
+		</AnimatePresence>
 	)
 }
 
